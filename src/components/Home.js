d2beab46496f7b75c94670da8775969080d705f5
Alexander-PC\Alexander <danielsson.alex@hotmail.com>
2018-11-25 00:28:52 +0100

    added code to changeaccount.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
-
 import withAuthorization from './withAuthorization';
 
+
+
+
 const HomePage = () =>
-  <div>
+    <div>
     <h1>Home Page</h1>
     <p>The Home Page is accessible by every signed in user.</p>
     <p>Good luck Stening</p>
+  
   </div>
+ 
 
 const authCondition = (authUser) => !!authUser;
-
 export default withAuthorization(authCondition)(HomePage);
